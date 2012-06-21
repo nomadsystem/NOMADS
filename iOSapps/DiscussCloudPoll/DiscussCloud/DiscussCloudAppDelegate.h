a9244a43e26ba64805130dcc0cedb3beff1eedf0
Steven Kemper <skemper@d-172-27-135-161.bootp.virginia.edu>
2012-06-21 15:50:41 -0400

    added iOS apps for the 100000th time

---
+++ iOSapps/DiscussCloudPoll/DiscussCloud/DiscussCloudAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  DiscussCloudAppDelegate.h
+//  DiscussCloud
+//
+//  Created by Steven Kemper on 5/15/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscussCloudAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+- (void) makeTabBar;
+
+@end
