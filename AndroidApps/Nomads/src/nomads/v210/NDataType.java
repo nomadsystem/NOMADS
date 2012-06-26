fbbda1edd6e266a35a22b69d7d24726d24043e6c
turowskipaul <turowski@nomads.hsd1.va.comcast.net>
2012-06-26 16:51:25 -0400

    added Android apps, revised PollStudent.java

---
+++ AndroidApps/Nomads/src/nomads/v210/NDataType.java
@@ -0,0 +1,11 @@
+package nomads.v210;
+
+
+public class NDataType {
+    public static byte FOO = 0;
+    public static byte BYTE = 1;
+    public static byte INT = 2;
+    public static byte FLOAT = 3;
+    public static byte DOUBLE = 4;
+    public static byte NO_DATA = 5;
+}
