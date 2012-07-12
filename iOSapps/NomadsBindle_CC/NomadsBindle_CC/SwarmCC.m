e84ba3462c5b291ec351f37a1173b9d3086f7ce5
PAT <turowskipaul@gmail.com>
2012-07-12 00:56:03 -0400

    ARC compatible

--- iOSapps/NomadsBindle_CC/NomadsBindle_CC/SwarmCC.m
+++ iOSapps/NomadsBindle_CC/NomadsBindle_CC/SwarmCC.m
@@ -96,13 +96,5 @@
 
 
 // on "dealloc" you need to release all your retained objects
-- (void) dealloc
-{
-	// in case you have something to dealloc, do it in this method
-	// in this particular example nothing needs to be released.
-	// cocos2d will automatically release all the children (Label)
-	
-	// don't forget to call "super dealloc"
-	[super dealloc];
-}
+- (void) dealloc {}
 @end
