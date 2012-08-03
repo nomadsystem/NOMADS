b8e51ecf05d6b9728c92112722f5d05e84fdd8d2
PAT <turowskipaul@gmail.com>
2012-08-03 17:25:03 -0400

    API 10; new graphics (background, icons)

--- AndroidApps/NomadsAuk/gen/com/nomads/R.java
+++ AndroidApps/NomadsAuk/gen/com/nomads/R.java
@@ -11,9 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int blue_ice_bg_320_480=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int nomadslogo_512=0x7f020002;
+        public static final int background=0x7f020000;
+        public static final int blue_ice_bg_320_480=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int nomadslogo_512=0x7f020003;
     }
     public static final class id {
         public static final int buttonCloud=0x7f06000b;
