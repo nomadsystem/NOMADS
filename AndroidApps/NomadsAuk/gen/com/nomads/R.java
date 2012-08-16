2a0c21c113733750319670156db09c1f9ff6e1b2
PAT <turowskipaul@gmail.com>
2012-08-16 16:50:03 -0400

    Nomads web link opens in app activity

--- AndroidApps/NomadsAuk/gen/com/nomads/R.java
+++ AndroidApps/NomadsAuk/gen/com/nomads/R.java
@@ -35,10 +35,10 @@ public final class R {
         public static final int connectionStatusPre=0x7f070009;
         public static final int dot=0x7f07000f;
         public static final int joinStatus=0x7f070006;
+        public static final int linkButton=0x7f07000c;
         public static final int message=0x7f070002;
         public static final int nomadsInfo=0x7f07000d;
-        public static final int nomadsInfo2=0x7f07000b;
-        public static final int nomadsLink=0x7f07000c;
+        public static final int nomadsLinkLayout=0x7f07000b;
         public static final int ok=0x7f070003;
         public static final int prompt=0x7f070001;
         public static final int relativelayout=0x7f070000;
@@ -65,11 +65,11 @@ public final class R {
         public static final int connectionStatus=0x7f050010;
         public static final int discuss=0x7f05000c;
         public static final int hint=0x7f050007;
+        public static final int linkButton=0x7f050013;
         public static final int login=0x7f050003;
         public static final int loginPrompt=0x7f050004;
         public static final int nomadsInfo=0x7f050011;
-        public static final int nomadsInfo2=0x7f050012;
-        public static final int nomadsLink=0x7f050013;
+        public static final int nomadsLink=0x7f050012;
         public static final int quitButton=0x7f050014;
         public static final int sendButton=0x7f050008;
         public static final int settings=0x7f05000f;
